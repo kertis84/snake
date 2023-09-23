ee9f28a86deaf90e15dd6cab813ceec5d0a8b39c
Kirill <kertis84@gmail.com>
2023-09-23 19:00:16 +0300

    add help menu

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import GameSettings, { LARGE_FIELD, MEDIUM_FIELD, SMALL_FIELD } from "./components/GameSettings";
+import GameSettings, {
+  LARGE_FIELD,
+  MEDIUM_FIELD,
+  SMALL_FIELD,
+} from "./components/GameSettings";
 import Copyright from "./components/Copyright.vue";
 import GameField from "./components/GameField.vue";
 import Gameplay from "./components/Gameplay";
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 
 const gameSettings = reactive(new GameSettings());
 const game = reactive(new Gameplay(gameSettings));
@@ -28,12 +32,19 @@ function resetGame(e: MouseEvent) {
     <div class="mx-2 menu-item my-auto" id="field_lg" @click="resetGame">
       Large
     </div>
-    <div class="mx-2 menu-item my-auto"></div>
+    <div class="mx-2 menu-item my-auto">
+      <div class="dropdown">
+        <div class="question"></div>
+        <div class="dropdown-content" >
+          <div class="dropdown-item">Управление: &larr; &uarr; &rarr; &darr;</div>
+          <div class="dropdown-item">Рестарт: space</div>
+          <div class="dropdown-item">Пауза: space</div>
+        </div>
+      </div>
+    </div>
   </div>
-  <div class="my-5 center score">
-    Score: {{ game.score }}
-  </div>
-  <GameField :settings="gameSettings" :game="game"/>
+  <div class="my-5 center score">Score: {{ game.score }}</div>
+  <GameField :settings="gameSettings" :game="game" />
   <Copyright />
 </template>
 
@@ -58,6 +69,10 @@ function resetGame(e: MouseEvent) {
   margin-left: 1rem;
   margin-right: 1rem;
 }
+.mx-5 {
+  margin-left: 2.5rem;
+  margin-right: 2.5rem;
+}
 .mx-auto {
   margin-left: auto;
   margin-right: auto;
@@ -72,5 +87,30 @@ function resetGame(e: MouseEvent) {
 .score {
   font-size: xx-large;
   font-weight: 600;
+}
+.question {
+  width: 30px;
+  height: 30px;
+  background-image: url(/images/question.svg);
+  background-size: 100% 100%;
+}
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: white;
+  margin-top: 10px;
+  min-width: 200px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  transform: translateX(-100px);
+}
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+.dropdown-item {
+  padding: 8px 16px;
 }
 </style>
